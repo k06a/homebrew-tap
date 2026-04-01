c70209cf6361f948b2e13c54a5bbf26b3da81033
Anton Bukov <k06aaa@gmail.com>
2026-04-01 21:46:12 +0000

    macpow: add 0.1.12 bottle.

--- Formula/macpow.rb
+++ Formula/macpow.rb
@@ -5,6 +5,12 @@ class Macpow < Formula
   sha256 "be0d5c056bbfc0379a6886d66f9f3bda70886315a2688bd07f1f7a4df54af4db"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/k06a/homebrew-tap/releases/download/macpow-0.1.12"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4e0ed2862978828908d67e6df3e72576c858798c00660b9de3d2d24d920317c1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d62e2a682af82681197d5f1f4bdeb8477ee754540bef8c6da8fd1d0b4dd49b9e"
+  end
+
   depends_on "rust" => :build
   depends_on arch: :arm64
   depends_on :macos
